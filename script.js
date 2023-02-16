6aaae148d78f71c063fe3b5bf3af98f33441ddd3
Andrei Beliakov <andrei@imd.ru>
2023-02-17 00:00:22 +0300

    done

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-// const response = await fetch("https://emoji.ymatuhin.workers.dev/");
-
 //ПОЛУЧАЕМ ДАННЫЕ С СЕРВЕРА
 const response = await fetch(
   "https://jsonplaceholder.typicode.com/posts/?_start=0&_limit=7"
 );
 const data = await response.json();
-// console.log(data[0].id);
-
 let selectArr = data;
-// console.log(selectArr);
+let selected = 0;
 
 const wrap = document.querySelector(".cards__wrapper");
 const card = document.querySelector(".card");
 const form = document.querySelector(".form");
 const checkbox = document.querySelector(".card__check");
+const count = document.querySelector(".selected");
 
 //ОТКЛЮЧЕНИЕ БАЗОВОГО ПОВЕДЕНИЯ ЭЛЕМЕНТОВ ФОРМЫ
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
 });
 
+//ПЕРЕКРАСКА ВЫБРАННОЙ ПО ЧЕКБОКСУ КАРТОЧКИ И ПОДСЧЕТ ВЫБРАННЫХ
 function invertCard(evnt) {
   evnt.currentTarget.parentElement.classList.toggle("invert");
-  // Когда данная функция используется в качестве обработчика события: this === e.currentTarget
+  if (evnt.currentTarget.checked) {
+    selected++;
+    count.innerHTML = `${selected}`;
+  } else {
+    selected--;
+    count.innerHTML = `${selected}`;
+  }
 }
-
-// function invertCard(id) {
-//   console.log(id);
-//   const card = document.querySelector(".card");
-// console.log(id);
-// if (checkbox.checked) checkedNum++;
-// else if (!checkbox.checked) checkedNum--;
-// console.log(checkedNum);
-//   card.classList.toggle("invert");
-// }
 
 //ОТРИСОВКА КАРТОЧКИ
 function createCard(obj) {
@@ -44,32 +38,19 @@ function createCard(obj) {
   checkbox.className = "card__check";
   newCard.innerHTML = `
 <h2 class="card__title">${obj.title}</h2>
-            <p class="card__text">${obj.body}</p>`;
-
-  // <input type="checkbox" class="card__check" />`;
-
-  // if (obj.userId === 1) {
-  // }
-
+<p class="card__text">${obj.body}</p>`;
   newCard.append(checkbox);
   checkbox.addEventListener("change", invertCard);
   return newCard;
 }
 
-//КОРРЕКТОРОВКА МАССИВА КАРТОЧЕК ПО ПОИСКУ И ВЫЗОВ ОТРИСОВКИ
+//КОРРЕКТОРОВКА МАССИВА КАРТОЧЕК ПО ПОИСКУ
 function searchFunction(event) {
   wrap.innerHTML = "";
   let value = event.target.value.trim().toLowerCase();
   selectArr
     .filter((elem) => elem.title.trim().toLowerCase().includes(value))
     .forEach((elem) => wrap.append(createCard(elem)));
-  // console.log(selectArr);
 }
 form.addEventListener("change", searchFunction);
 selectArr.forEach((newCard) => wrap.append(createCard(newCard)));
-
-// checkbox.addEventListener("change", invertCard);
-// const checkbox = document.querySelector(".card__check");
-// checkbox.addEventListener("change", () => console.log("click"));
-// checkbox.addEventListener("change", invertCard);
-//ОБРАБОТКА МАССИВА ТУДУШЕК ПРИ НАЖАТИИ НА CHECKBOX
